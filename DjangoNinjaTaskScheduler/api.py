615455955aedb0dae4a25f980bd650f962c1bcce
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-12 17:10:25 +0400

    Реализация аутенфикации по JWT токену. Реализаци приложения задач.

--- DjangoNinjaTaskScheduler/api.py
+++ DjangoNinjaTaskScheduler/api.py
@@ -1,9 +1,9 @@
-from ninja_extra import NinjaExtraAPI
+from ninja import NinjaAPI
 
 from auth.jwt_auth_model import JWTAuthRequired
 from auth.views import router as auth_router
 from tasks.views import router as tasks_router
 
-api = NinjaExtraAPI()
+api = NinjaAPI()
 api.add_router("/tasks/", tasks_router, tags=["tasks"], auth=JWTAuthRequired)
 api.add_router("/auth/", auth_router, tags=["auth"])
