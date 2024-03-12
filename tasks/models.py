a98e639a28808c3cf84e134016b4d0558e4e492a
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-12 16:01:25 +0400

    Добавление поля времени создания и апдейта задачи

--- tasks/models.py
+++ tasks/models.py
@@ -8,3 +8,5 @@ class Task(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название', db_index=True)
     description = models.TextField(max_length=10000, verbose_name='Описание')
     status = models.CharField(max_length=255, choices=TaskStatuses.choices())
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
