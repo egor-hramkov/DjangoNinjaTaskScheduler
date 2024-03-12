0516d0922f61f6389dfd05f1104e39b56bdae7da
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-12 16:02:54 +0400

    Добавление наименований в модель

--- tasks/models.py
+++ tasks/models.py
@@ -7,6 +7,6 @@ class Task(models.Model):
     """Модель задачи"""
     name = models.CharField(max_length=200, verbose_name='Название', db_index=True)
     description = models.TextField(max_length=10000, verbose_name='Описание')
-    status = models.CharField(max_length=255, choices=TaskStatuses.choices())
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=255, choices=TaskStatuses.choices(), verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания задачи')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления задачи')
