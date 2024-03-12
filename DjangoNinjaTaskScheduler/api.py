25f6c455520bdbdc7c1280d9611dc1618c9d5a4f
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-12 17:51:49 +0400

    auth fix

--- DjangoNinjaTaskScheduler/api.py
+++ DjangoNinjaTaskScheduler/api.py
@@ -1,9 +1,10 @@
 from ninja import NinjaAPI
+from ninja_jwt.authentication import JWTAuth
 
 from auth.jwt_auth_model import JWTAuthRequired
 from auth.views import router as auth_router
 from tasks.views import router as tasks_router
 
 api = NinjaAPI()
-api.add_router("/tasks/", tasks_router, tags=["tasks"], auth=JWTAuthRequired())
+api.add_router("/tasks/", tasks_router, tags=["tasks"], auth=JWTAuth())
 api.add_router("/auth/", auth_router, tags=["auth"])
