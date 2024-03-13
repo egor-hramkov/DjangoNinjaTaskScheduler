545c078dabc055ff78dd9c0bfd5c69bc826ed244
Egor <egor_kuplinov@mail.ru>
2024-03-13 17:33:28 +0400

    Переделана JWT авторизация

--- DjangoNinjaTaskScheduler/api.py
+++ DjangoNinjaTaskScheduler/api.py
@@ -1,10 +1,9 @@
-from ninja import NinjaAPI
+from ninja_extra import NinjaExtraAPI
 from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
 
-from auth.jwt_auth_model import JWTAuthRequired
-from auth.views import router as auth_router
 from tasks.views import router as tasks_router
 
-api = NinjaAPI()
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
 api.add_router("/tasks/", tasks_router, tags=["tasks"], auth=JWTAuth())
-api.add_router("/auth/", auth_router, tags=["auth"])
--- DjangoNinjaTaskScheduler/settings.py
+++ DjangoNinjaTaskScheduler/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ninja_jwt',
+    'ninja_extra',
     'tasks',
 ]
 
