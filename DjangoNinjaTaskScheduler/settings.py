13e913ab2924e73e26a54301becd2209f8b0765d
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-14 16:35:10 +0400

    Добавление Джанго админки

--- DjangoNinjaTaskScheduler/settings.py
+++ DjangoNinjaTaskScheduler/settings.py
@@ -18,7 +18,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ SECRET_KEY = 'django-insecure-in+w!@$bfz83dx=w=5($&h+&0mkqovr3fe17*4x$*(!p0%prxu
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'DjangoNinjaTaskScheduler.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -90,7 +87,6 @@ DATABASES = {
         'PORT': os.environ['PORT'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -110,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -122,12 +117,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- DjangoNinjaTaskScheduler/urls.py
+++ DjangoNinjaTaskScheduler/urls.py
@@ -1,9 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from DjangoNinjaTaskScheduler import settings
 from DjangoNinjaTaskScheduler.api import api
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
