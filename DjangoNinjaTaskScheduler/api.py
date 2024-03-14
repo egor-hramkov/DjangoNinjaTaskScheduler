ff56212b5dd362558eb3970a3546bdd84e0d92cb
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-14 15:13:24 +0400

    Подключение логгера

--- DjangoNinjaTaskScheduler/api.py
+++ DjangoNinjaTaskScheduler/api.py
@@ -1,9 +1,18 @@
+import logging
+
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.authentication import JWTAuth
 from ninja_jwt.controller import NinjaJWTDefaultController
 
 from tasks.views.task_view import router as tasks_router
 from users.views.user_view import router as user_router
+
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(pathname)s at line %(lineno)d | %(message)s",
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 api = NinjaExtraAPI()
 
--- users/ordering/user_ordering.py
+++ users/ordering/user_ordering.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.core.exceptions import FieldError
 from django.db.models import QuerySet
 from ninja import Schema
+
+logger = logging.getLogger(__name__)
 
 
 class UserOrderingEntity(Schema):
@@ -11,5 +15,5 @@ class UserOrderingEntity(Schema):
         try:
             qs = qs.order_by(self.ordering_field)
         except FieldError:
-            pass
+            logger.info(f"Передано несуществующее значение {self.ordering_field} для сортировки списка пользователя!")
         return qs
