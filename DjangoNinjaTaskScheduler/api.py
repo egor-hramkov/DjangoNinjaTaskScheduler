c0fffd86280e9b8b2a34f3294525dc9f5b950925
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-15 10:52:45 +0400

    Асинхронная JWTAuth

--- DjangoNinjaTaskScheduler/api.py
+++ DjangoNinjaTaskScheduler/api.py
@@ -1,7 +1,7 @@
 import logging
 
 from ninja_extra import NinjaExtraAPI
-from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.authentication import AsyncJWTAuth
 from ninja_jwt.controller import NinjaJWTDefaultController
 
 from tasks.views.task_view import router as tasks_router
@@ -18,5 +18,5 @@ api = NinjaExtraAPI()
 
 api.register_controllers(NinjaJWTDefaultController)
 
-api.add_router("/tasks/", tasks_router, tags=["tasks"], auth=JWTAuth())
+api.add_router("/tasks/", tasks_router, tags=["tasks"], auth=AsyncJWTAuth())
 api.add_router("/users/", user_router, tags=["users"])
