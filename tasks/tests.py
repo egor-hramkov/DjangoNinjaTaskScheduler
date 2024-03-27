c70cbd72d37c486b19ab5fa160aeeaeb86a2db84
Егор Храмков <egor_kuplinov@mail.ru>
2024-03-27 09:56:42 +0400

    Написание тестов для модуля пользователей и задач

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,48 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 
-# Create your tests here.
+from ninja_jwt.tokens import AccessToken
+
+from tasks.models import Task
+
+
+class TaskTests(TestCase):
+    """Тесты для раздела задач"""
+    c = Client()
+    BASE_URL = "/api/tasks/"
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_for_task', password='abcdefasd', email="task@mail.ru")
+        self.user_id = self.user.id
+        self.task = Task.objects.create(name="test_task", description="teeeest", status='created', user=self.user)
+        self.task_id = self.task.id
+        self.token = AccessToken.for_user(self.user)
+        self.task_data = {
+            "name": "test_task",
+            "description": "test_task",
+            "status": "created",
+        }
+
+    def test_get_task(self):
+        """Тест API на создание задачи"""
+        response = self.c.get(f"{self.BASE_URL}{self.task_id}", HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        response_data = response.json()
+        assert response.status_code == 200
+        assert response_data['id'] == self.task_id
+
+    def test_create_task(self):
+        """Тест API на создание задачи"""
+        response = self.c.post(
+            f"{self.BASE_URL}",
+            self.task_data,
+            HTTP_AUTHORIZATION=f'Bearer {self.token}',
+            content_type="application/json",
+        )
+        response_data = response.json()
+        assert response.status_code == 200
+        assert response_data['name'] == self.task_data['name']
+        assert response_data['user']['id'] == self.user.id
+
+    def test_update_task(self):
+        """Тест API на обновление задачи"""
+        ...
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
 
-# Create your tests here.
+class UserTests(TestCase):
+    """Тесты для приложения пользователя"""
+    BASE_URL = "/api/users/"
+    c = Client()
+    create_user_data = {
+        "username": "test123",
+        "email": "user@example.com",
+        "first_name": "string",
+        "last_name": "string",
+        "password": "string"
+    }
+
+    def setUp(self):
+        self.user_id = User.objects.create_user(username='test', password='test_password', email="test@mail.ru").id
+
+    def test_get_user(self):
+        """Тест на получение пользователя"""
+        response = self.c.get(f"{self.BASE_URL}{self.user_id}")
+        response_data = response.json()
+        assert response.status_code == 200
+        assert response_data['id'] == self.user_id
+
+    def test_create_user(self):
+        """Тест на создание пользователя"""
+        response = self.c.post(self.BASE_URL, self.create_user_data, content_type="application/json")
+        response_data = response.json()
+        assert response.status_code == 200
+        assert response_data['email'] == self.create_user_data['email']
